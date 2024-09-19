bdabda8ec59187f95e9d21cded9fd3f4e98abb82
Cindy Zhou <03cindy.zhou@gmail.com>
2024-09-19 09:32:21 +0300

    aliisan versio

--- src/main/java/DAO/UserDao.java
+++ src/main/java/DAO/UserDao.java
@@ -3,7 +3,7 @@ package DAO;
 import Model.User;
 
 public interface UserDao {
-    void createUser(User user);
+    boolean createUser(User user);
     User getUser(String username);
     // Other methods related to User database operations
 
