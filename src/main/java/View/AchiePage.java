76406d3e27e3ab31750eb40ea61025718c29dc97
Cindy Zhou <03cindy.zhou@gmail.com>
2024-09-30 19:11:33 +0300

    Added a badge, editing achievement page

--- src/main/java/View/AchiePage.java
+++ src/main/java/View/AchiePage.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import Controller.UserController;
@@ -20,11 +21,13 @@ public class AchiePage extends BasePage implements ImageSize, setMarginButton, b
     private VBox unlockedBadgesContainer;
     private VBox lockedBadgesContainer;
 
+    //Displays the Achievements page
     public AchiePage(Stage stage) {
         this.userController = UserController.getInstance(UserDaoImpl.getInstance());
         this.badges = new ArrayList<>();
         badges.add(new Badge("file:src/main/resources/FirstFlashBadge.png", 1));
         badges.add(new Badge("file:src/main/resources/SecondBadge.png", 5));
+        badges.add(new Badge("file:src/main/resources/ThirdBadge.png", 10));
 
         String userEmail = userController.getEmailDao();
 
@@ -42,26 +45,46 @@ public class AchiePage extends BasePage implements ImageSize, setMarginButton, b
         unlockedBadgesContainer = new VBox();
         lockedBadgesContainer = new VBox();
 
-        for (Badge badge : badges) {
-            ImageView imageView = new ImageView(badge.getImage());
-            imageView.setPreserveRatio(true);
-            setImageSize(imageView, 100, 100);
-            VBox.setMargin(imageView, new Insets(10, 10, 10, 5));
-
-            int quizzesCompleted = userController.getQuizzesCompleted(userEmail);
-            if (quizzesCompleted >= badge.getThreshold()) {
-                unlockBadge(true, imageView);
-                unlockedBadgesContainer.getChildren().add(imageView);
-            } else {
-                lockBadge(true, imageView);
-                lockedBadgesContainer.getChildren().add(imageView);
-            }
-        }
+        addBadgesToContainer(unlockedBadgesContainer, userEmail, true);
+        addBadgesToContainer(lockedBadgesContainer, userEmail, false);
 
         Label AchieLabel2 = new Label("Earned badges");
         Label AchieLabel3 = new Label("Locked badges");
 
         this.getChildren().addAll(AchieLabel2, unlockedBadgesContainer, AchieLabel3, lockedBadgesContainer);
+    }
+
+    //Adds badges to the container
+    private void addBadgesToContainer(VBox container, String userEmail, boolean unlocked) {
+        HBox currentRow = null;
+        int badgeCount = 0;
+
+        //Iterates through the badges and adds them to the container
+        for (Badge badge : badges) {
+            //If the badge count is even, a new row is created
+            if (badgeCount % 2 == 0) {
+                currentRow = new HBox(10);
+                VBox.setMargin(currentRow, new Insets(10, 10, 10, 5));
+                container.getChildren().add(currentRow);
+            }
+
+            ImageView imageView = new ImageView(badge.getImage());
+            imageView.setPreserveRatio(true);
+            setImageSize(imageView, 130, 130);
+
+            int quizzesCompleted = userController.getQuizzesCompleted(userEmail);
+
+            //If the badge is unlocked and the user has completed the required number of quizzes, the badge is unlocked
+            if (unlocked && quizzesCompleted >= badge.getThreshold()) {
+                unlockBadge(true, imageView);
+                currentRow.getChildren().add(imageView);
+            } else if (!unlocked && quizzesCompleted < badge.getThreshold()) {
+                lockBadge(true, imageView);
+                currentRow.getChildren().add(imageView);
+            }
+
+            badgeCount++;
+        }
     }
 
     @Override
@@ -83,6 +106,7 @@ public class AchiePage extends BasePage implements ImageSize, setMarginButton, b
             imageView.setEffect(desaturate);
         }
     }
+
     @Override
     public void lockBadge(boolean value, ImageView imageView) {
         if (value) {
