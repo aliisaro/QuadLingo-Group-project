5da9158860ffa9027d78cd098c2a98aa3986d307
Cindy Zhou <03cindy.zhou@gmail.com>
2024-10-08 18:13:19 +0300

    Deleted unnecessary classes

--- src/main/java/DAO/AnswerDao.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import Model.Answer;
-
-public interface AnswerDao {
-    void createAnswer(Answer answer);
-    Answer getAnswer(String answer);
-    // Other methods related to Answer database operations
-}
--- src/main/java/DAO/QuestionDao.java
+++
@@ -1,9 +0,0 @@
-package DAO;
-
-import Model.Question;
-
-public interface QuestionDao {
-    void createQuestion(Question question);
-    Question getQuestion(String question);
-    // Other methods related to Question database operations
-}
