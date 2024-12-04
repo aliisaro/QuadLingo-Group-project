b2e3cb79d2296788b51c5c63415316c6eb825f54
maisajulianna <maisa.osterman@gmail.com>
2024-12-04 22:37:37 +0200

    more sonarlint changes

--- src/main/java/Database/MariaDbConnection.java
+++ src/main/java/Database/MariaDbConnection.java
@@ -4,19 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.logging.Logger;
 
 public class MariaDbConnection {
   private static final Dotenv dotenv = Dotenv.load();
   private static final String URL = "jdbc:mariadb://10.120.32.98/QuadLingo";
   private static final String USER = dotenv.get("DB_USER");
   private static final String PASSWORD = dotenv.get("DB_PASSWORD");
+  private static final Logger LOGGER = Logger.getLogger(MariaDbConnection.class.getName());
+
 
   public static Connection getConnection() {
     Connection conn = null;
     try {
       conn = DriverManager.getConnection(URL, USER, PASSWORD);
     } catch (SQLException e) {
-      System.out.println("Connection failed.");
+      LOGGER.info("Connection failed.");
       e.printStackTrace();
     }
     return conn;
