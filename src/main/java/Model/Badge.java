10f6c39dd062c14e5ce7f82b2f588fa53405f8cb
Yasmin Belhout <yasmin.belhout@metropolia.fi>
2024-12-04 12:38:08 +0200

    Badge checkstyle fix

--- src/main/java/Model/Badge.java
+++ src/main/java/Model/Badge.java
@@ -3,31 +3,31 @@ package Model;
 import javafx.scene.image.Image;
 
 public class Badge {
-    private final Image image;
-    private final int threshold;
-    private final String description;
-    private final String checker;
+  private final Image image;
+  private final int threshold;
+  private final String description;
+  private final String checker;
 
-    public Badge(String imagePath, int threshold, String description, String checker) {
-        this.image = new Image(imagePath);
-        this.threshold = threshold;
-        this.description = description;
-        this.checker = checker;
-    }
+  public Badge(String imagePath, int threshold, String description, String checker) {
+    this.image = new Image(imagePath);
+    this.threshold = threshold;
+    this.description = description;
+    this.checker = checker;
+  }
 
-    public String getChecker() {
-        return checker;
-    }
+  public String getChecker() {
+    return checker;
+  }
 
-    public Image getImage() {
-        return image;
-    }
+  public Image getImage() {
+    return image;
+  }
 
-    public int getThreshold() {
-        return threshold;
-    }
+  public int getThreshold() {
+    return threshold;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public String getDescription() {
+    return description;
+  }
 }
